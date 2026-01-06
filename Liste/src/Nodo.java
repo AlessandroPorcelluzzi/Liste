c4926b4aac76b11694f9e7b4c84da4bd4666d835
AlessandroPorcelluzzi <alessandro.porcelluzzi@aldini.istruzioneer.it>
2026-01-06 16:26:54 +0100

    aggiunto il prev

--- Liste/src/Nodo.java
+++ Liste/src/Nodo.java
@@ -1,15 +1,19 @@
 public class Nodo {
     private String value;
     private Nodo next;
+    private Nodo prev;
 
-    public Nodo (String v) {
-        value=v;
+    public Nodo(String v) {
+        value = v;
         next = null;
+        prev = null;
     }
-
-    public void setNext (Nodo n) {next = n;}
+    
+    public void setNext (Nodo n) {next = n;}   
     public Nodo getNext() {return next;}
     public void setValue (String v) {value = v;}
     public String getValue() {return value;}
+    public void setPrev(Nodo p) {prev=p;}
+    public Nodo getPrev() {return prev;}
+    
 }
-
