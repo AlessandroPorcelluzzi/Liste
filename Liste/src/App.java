1e81392e4b355609d9a66b2e9e36a8c27efb7625
AlessandroPorcelluzzi <alessandro.porcelluzzi@aldini.istruzioneer.it>
2026-01-06 16:42:48 +0100

    Update App.java
    Added methods to print the list in reverse and check for existence of nodes.

--- Liste/src/App.java
+++ Liste/src/App.java
@@ -1,10 +1,23 @@
 public class App {
-    public static void main (String[] args){
-        Lista l= new Lista();
-        l.addTail(new Nodo ("mario"));
-        l.addTail(new Nodo ("giulio"));
-        l.addHead(new Nodo ("francesco"));
+    public static void main(String[] args) {
+
+        Lista l = new Lista();
+
+        l.addTail(new Nodo("mario"));
+        l.addTail(new Nodo("giulio"));
+        l.addHead(new Nodo("francesco"));
         l.add("juve magica juve");
-        System.out.println (l);
-     }
+
+        System.out.println(l);
+        System.out.println(l.toReverseString());
+
+        System.out.println("Esiste 'mario'? " + l.exists("mario"));
+        System.out.println("Esiste 'anna'? " + l.exists("anna"));
+
+        System.out.println("Rimuovo 'giulio'");
+        l.remove("giulio");
+
+        System.out.println(l);
+        System.out.println(l.toReverseString());
+    }
 }
